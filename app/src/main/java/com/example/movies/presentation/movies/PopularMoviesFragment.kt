7b9640a779ebe64df52f137b3c1fe8f8620d2555
Atef <mohamed.atef@shgardi.app>
2023-09-09 16:35:01 +0300

    Preparing app theme

---
+++ app/src/main/java/com/example/movies/presentation/movies/PopularMoviesFragment.kt
@@ -0,0 +1,28 @@
+package com.example.movies.presentation.movies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.movies.databinding.FragmentPopularMoviesBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PopularMoviesFragment : Fragment() {
+
+    private lateinit var binding: FragmentPopularMoviesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPopularMoviesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
