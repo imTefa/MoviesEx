c01a0e930f47bca7b03f7af124afe33cd7f261fd
Atef <mohamed.atef@shgardi.app>
2023-09-09 15:40:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+//7b4195d4bf17d46b804bf8ee8c316911
+//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjQxOTVkNGJmMTdkNDZiODA0YmY4ZWU4YzMxNjkxMSIsInN1YiI6IjY0ZmM2MjZjZTBjYTdmMDEyZWI3MDlmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YAVmZmjjVVzHJm1QZLiQ2OEr8Qx-6HutQdB1G3GTJ0M
